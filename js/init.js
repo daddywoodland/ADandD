e32e32e437f10d606b4ffbf787e007422d2d3130
daddywoodland <arwoods@gmail.com>
2015-02-01 10:46:51 +0000

    Design changes (colour mainly) Include case study link

--- js/init.js
+++ js/init.js
@@ -4,5 +4,22 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+      var fixedNav = $('.navbar-fixed')[0];
+      var offsetFixeNavHeight = fixedNav == undefined ? 0 : fixedNav.clientHeight;
+      offsetFixeNavHeight = (offsetFixeNavHeight == undefined ? 0 : offsetFixeNavHeight);
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - offsetFixeNavHeight
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
